8c84cdd00d8daca0ac75ab225bce953fae2a2108
Veselin <vesso100@gmail.com>
2021-11-02 14:36:05 +0100

    def render_register_screen added

--- screens.py
+++ screens.py
@@ -1,20 +1,52 @@
 import tkinter as tk
 
 
-def render_main_screen(window):
-    login_button = tk.Button(
+def clear_window(window: tk.Tk):
+    for widget in window.winfo_children():
+        widget.destroy()
+
+
+def render_main_screen(window: tk.Tk):
+    tk.Button(
         window,
         text='Login',
         bg='green',
         fg='white',
-        command=lambda: print('Login clicked'))
+        command=lambda: render_login_screen(window)
+    ).grid(row=0, column=0)
 
-    register_button = tk.Button(
+    tk.Button(
         window,
         text='Register',
         bg='yellow',
         fg='black',
-        command=lambda: print('reg clicked'))
+        command=lambda: render_register_screen(window)
+    ).grid(row=0, column=1)
 
-    login_button.grid(row=0, column=0)
-    register_button.grid(row=0, column=1)
+
+def render_register_screen(window: tk.Tk):
+    clear_window(window)
+
+    username = tk.Entry(window)
+    username.grid(row=0, column=0)
+
+    password = tk.Entry(window, show='*')
+    password.grid(row=1, column=0)
+
+    first_name = tk.Entry(window)
+    first_name.grid(row=2, column=0)
+
+    last_name = tk.Entry(window)
+    last_name.grid(row=3, column=0)
+
+    tk.Button(
+        window,
+        text='Register',
+        bg='green',
+        fg='black',
+        command=lambda: print(username.get(), password.get())
+    ).grid(row=4, column=0)
+
+
+def render_login_screen(window: tk.Tk):
+    clear_window(window)
